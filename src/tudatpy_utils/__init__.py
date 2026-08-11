4263d0eba8dacd083aba8e90d9749969772c4c89
Jeongyun Lee <jeongyun@gmail.com>
2026-08-11 00:24:09 -0400

    Move to src/tudatpy_utils/

--- src/tudatpy_utils/__init__.py
+++ src/tudatpy_utils/__init__.py
@@ -5,6 +5,30 @@ import sys
 
 __version__ = "0.1.0"
 
-for _package_name in ("common", "diff_oem", "oem_to_omm", "propagation", "plotting"):
-	_package = importlib.import_module(_package_name)
-	sys.modules[f"{__name__}.{_package_name}"] = _package
+from . import common as _common
+sys.modules["common"] = _common
+
+from .common import ccsds as _ccsds
+from .common import interpolator as _interpolator
+
+sys.modules["common.ccsds"] = _ccsds
+sys.modules["common.interpolator"] = _interpolator
+
+for _package_name in (
+	"diff_oem",
+	"download_tle",
+	"oem_to_omm",
+	"omm_to_tle",
+	"plot_dep_vars",
+	"plot_orbit",
+	"plot_orbit_deltas",
+	"propagate_kepler",
+	"propagate_orbit",
+	"propagate_tle",
+	"slice_oem",
+	"tle_info",
+	"tle_to_omm",
+	"xform_oem",
+):
+	_module = importlib.import_module(f".{_package_name}", __name__)
+	sys.modules[_package_name] = _module
