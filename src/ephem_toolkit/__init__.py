87cd442e38cb6c184cf3854c3b21a0e4085a3888
Jeongyun Lee <jeongyun@gmail.com>
2026-08-16 20:40:42 -0400

    Unused file

--- src/ephem_toolkit/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Python utilities for TudatPy astrodynamics workflows."""
-
-from .evaluate_interpolator_accuracy import evaluate_boundary_accuracy
-
-__version__ = "0.1.0"
-
-__all__ = ["evaluate_boundary_accuracy"]
